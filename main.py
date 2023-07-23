8fdb9bbf64fed91eaee1cbc73e93c1cfb9cac9db
michaelsatumba <mjsatumba@gmail.com>
2023-07-23 14:34:30 -0700

    security check is stopping program

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ password = os.getenv("PASSWORD")
 # print(username, password)
 # change the job title to whatever you want to search for
 job_title = "Software Engineer"
+# change the location to whatever you want to search for
+location = "California, United States"
 
 # Setup web driver
 options = Options()
@@ -27,6 +29,7 @@ options = Options()
 browser = webdriver.Chrome(options=options)
 # Wait for the login process to complete
 browser.implicitly_wait(10)  # Adjust the wait time as needed
+browser.set_window_size(1500, 800)
 
 # Open LinkedIn login page
 def login(browser, username, password):
@@ -37,28 +40,40 @@ def login(browser, username, password):
     print("Logged in")
 
 def click_on_jobs_tab(browser):
-    browser.find_element(By.XPATH, "/html/body/div[5]/header/div/nav/ul/li[3]/a/span").click()
+    browser.find_element(By.CSS_SELECTOR, "#global-nav > div > nav > ul > li:nth-child(3) > a").click()
     print("Clicked on jobs tab")
     # time.sleep(5)
     
-def click_on_search_bar(browser):
-    browser.find_element(By.XPATH, "/html/body/div[5]/header/div/nav/ul/li[3]/a").click()
-    print("Clicked on search bar")
-    # time.sleep(5)
     
 def search_for_job(browser, job_title):
-    search_box = browser.find_element(By.XPATH, '/html/body/div[5]/header/div/div/div/div[2]/div[2]/div/div/input[1]')
-    search_box.send_keys(job_title)
-    search_box.send_keys(Keys.ENTER)
-    # time.sleep(5)
+    search_box_job = browser.find_element(By.XPATH, '/html/body/div[5]/header/div/div/div/div[2]/div[2]/div/div/input[1]')
+    search_box_job.send_keys(job_title)
+    search_box_job.send_keys(Keys.ENTER)
+    time.sleep(5)
+  
+def search_for_location(browser, location):
+    search_box_location = browser.find_element(By.XPATH, '/html/body/div[5]/header/div/div/div/div[2]/div[2]/div/div/input[1]')
+    # Clear the input field first to empty it
+    search_box_location.clear()
+    # Enter the new location
+    search_box_location.send_keys(location)
+    # Perform any other actions if needed, like pressing Enter or clicking the search button
+    browser.find_element(By.XPATH, "/html/body/div[5]/header/div/div/div/div[2]/button[1]").click()
+    time.sleep(5)
+    print("Searched for location")
     
+def click_on_easy_apply(browser):
+    browser.find_element(By.XPATH, "/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/ul/li[8]/div/button").click()
+    time.sleep(5)
     
-    
-# Login function
+
 login(browser, username, password)
 click_on_jobs_tab(browser)
-click_on_search_bar(browser)
+# click_on_search_bar_job(browser)
 search_for_job(browser, job_title)
+# click_on_search_bar_location(browser)
+search_for_location(browser, location)
+click_on_easy_apply(browser)
 
 
 # Close the browser
